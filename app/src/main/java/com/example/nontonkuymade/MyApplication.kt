34dae30984fc45053c636f0a3ee76f22ef4860ad
lordrians <ferrianrp@gmail.com>
2021-03-08 04:05:15 +0700

    - Nambahin Leak Canary - Ganti pakai MultiDexApplication karne error kalau pakai leak canary

--- app/src/main/java/com/example/nontonkuymade/MyApplication.kt
+++ app/src/main/java/com/example/nontonkuymade/MyApplication.kt
@@ -1,10 +1,11 @@
 package com.example.nontonkuymade
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class MyApplication : Application(){
+open class MyApplication : MultiDexApplication() {
 //    private val coreComponent: CoreComponent by lazy {
 //        DaggerCoreComponent.factory().create(applicationContext)
 //    }
